0bf0718e55f0c8929aff04e37761d4f6cd9c81e5
Peyton Chandarana <peytonchandarana@gmail.com>
2023-05-08 02:52:18 -0400

    Init the build.sh script.

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-# TODO: This script will build all of the docker images
-#       and push the images to DockerHub.
+# Create docker build environment
+docker buildx create --name ubuntu_env --use
+
+# Base
+docker buildx build --no-cache --tag s7117/ubuntu_base:latest --platform=linux/amd64,linux/arm64 --push base
+
+# CUDA
+docker buildx build --no-cache --tag s7117/ubuntu_cuda:latest --platform=linux/amd64 --push cuda
+
+# ML
+docker buildx build --no-cache --tag s7117/ubuntu_ml:latest --platform=linux/amd64 --push ml
+
+# OpenVINO
+docker buildx build --no-cache --tag s7117/ubuntu_openvino:latest --platform=linux/amd64,linux/arm64 --push openvino
