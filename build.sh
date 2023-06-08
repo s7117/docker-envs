37d08ea5ab8bdefeddbeaf3a4acd48950c41ff7d
Peyton Chandarana <peytonchandarana@gmail.com>
2023-06-07 23:36:21 -0400

    Added tag after push in command.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "### LOG --> Created build environment..."
 
 # Base
 echo "### LOG --> BUILDING BASE IMAGE..."
-docker buildx build -f ./base/Dockerfile --no-cache --tag s7117/ubuntu_base:latest --platform=linux/amd64,linux/arm64 --push base
+docker buildx build -f ./base/Dockerfile --no-cache --platform=linux/amd64,linux/arm64 --push --tag s7117/ubuntu_base:latest
 
 echo "### LOG --> Base image built..."
 echo "### LOG --> Continue to build?"
@@ -15,9 +15,9 @@ read TEMPANS
 
 # CUDA
 echo "### LOG --> BUILDING CUDA IMAGES..."
-docker buildx build -f ./cuda/11.7/Dockerfile --no-cache --tag s7117/ubuntu_cuda:11.7 --platform=linux/amd64 --push cuda
-docker buildx build -f ./cuda/11.8/Dockerfile --no-cache --tag s7117/ubuntu_cuda:11.8 --platform=linux/amd64 --push cuda
-docker buildx build -f ./cuda/12.1/Dockerfile --no-cache --tag s7117/ubuntu_cuda:12.1 --platform=linux/amd64 --push cuda
+docker buildx build -f ./cuda/11.7/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_cuda:11.7
+docker buildx build -f ./cuda/11.8/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_cuda:11.8
+docker buildx build -f ./cuda/12.1/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_cuda:12.1
 
 echo "### LOG --> CUDA images built..."
 echo "### LOG --> Continue to build?"
@@ -25,9 +25,9 @@ read TEMPANS
 
 # ML
 echo "### LOG --> BUILDING ML IMAGES..."
-docker buildx build -f ./ml/11.7/Dockerfile --no-cache --tag s7117/ubuntu_ml:11.7 --platform=linux/amd64 --push ml
-docker buildx build -f ./ml/11.8/Dockerfile --no-cache --tag s7117/ubuntu_ml:11.8 --platform=linux/amd64 --push ml
-docker buildx build -f ./ml/12.1/Dockerfile --no-cache --tag s7117/ubuntu_ml:12.1 --platform=linux/amd64 --push ml
+docker buildx build -f ./ml/11.7/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_ml:11.7
+docker buildx build -f ./ml/11.8/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_ml:11.8
+docker buildx build -f ./ml/12.1/Dockerfile --no-cache --platform=linux/amd64 --push --tag s7117/ubuntu_ml:12.1
 
 echo "### LOG --> ML images built..."
 echo "### LOG --> Continue to build?"
