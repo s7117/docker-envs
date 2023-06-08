06799d85fb70eaddd6aa0dfc9045184574961b21
Peyton Chandarana <peytonchandarana@gmail.com>
2023-06-07 22:54:03 -0400

    Update base, cuda, and ml dockerfiles. Implemented new build script.

--- build.sh
+++ build.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
 # Create docker build environment
-docker buildx create --name ubuntu_env --use
+docker buildx create --name ubuntu_envs --use
+
+echo "### LOG --> Created build environment..."
 
 # Base
-docker buildx build --no-cache --tag s7117/ubuntu_base:latest --platform=linux/amd64,linux/arm64 --push base
+echo "### LOG --> BUILDING BASE IMAGE..."
+docker buildx build -f ./base/Dockerfile --no-cache --tag s7117/ubuntu_base:latest --platform=linux/amd64,linux/arm64 --push base
+
+echo "### LOG --> Base image built..."
+echo "### LOG --> Continue to build?"
+read TEMPANS
 
 # CUDA
-docker buildx build --no-cache --tag s7117/ubuntu_cuda:latest --platform=linux/amd64 --push cuda
+echo "### LOG --> BUILDING CUDA IMAGES..."
+docker buildx build -f ./cuda/11.7/Dockerfile --no-cache --tag s7117/ubuntu_cuda:11.7 --platform=linux/amd64 --push cuda
+docker buildx build -f ./cuda/11.8/Dockerfile --no-cache --tag s7117/ubuntu_cuda:11.8 --platform=linux/amd64 --push cuda
+docker buildx build -f ./cuda/12.1/Dockerfile --no-cache --tag s7117/ubuntu_cuda:12.1 --platform=linux/amd64 --push cuda
+
+echo "### LOG --> CUDA images built..."
+echo "### LOG --> Continue to build?"
+read TEMPANS
 
 # ML
-docker buildx build --no-cache --tag s7117/ubuntu_ml:latest --platform=linux/amd64 --push ml
+echo "### LOG --> BUILDING ML IMAGES..."
+docker buildx build -f ./ml/11.7/Dockerfile --no-cache --tag s7117/ubuntu_ml:11.7 --platform=linux/amd64 --push ml
+docker buildx build -f ./ml/11.8/Dockerfile --no-cache --tag s7117/ubuntu_ml:11.8 --platform=linux/amd64 --push ml
+docker buildx build -f ./ml/12.1/Dockerfile --no-cache --tag s7117/ubuntu_ml:12.1 --platform=linux/amd64 --push ml
+
+echo "### LOG --> ML images built..."
+echo "### LOG --> Continue to build?"
+read TEMPANS
+
+# Minecraft
+# TODO
 
 # OpenVINO
-docker buildx build --no-cache --tag s7117/ubuntu_openvino:latest --platform=linux/amd64,linux/arm64 --push openvino
+# FIXME
+# docker buildx build --no-cache --tag s7117/ubuntu_openvino:latest --platform=linux/amd64,linux/arm64 --push openvino
+
+# Cleanup build environment
+docker buildx rm ubuntu_envs
--- cuda/README.md
+++ cuda/README.md
@@ -1,22 +1,27 @@
 # Ubuntu CUDA Docker Development Environment
-*** Be sure to change the password for the user and root immediately after creating the container! ***
+
+**_ Be sure to change the password for the user and root immediately after creating the container! _**
 
 # CUDA
-*** You must use the `--gpus all` argument to the `docker run` command to pass gpus access to the containter. ***
+
+**_ You must use the `--gpus all` argument to the `docker run` command to pass gpus access to the containter. _**
 
 Before using this image please refer to the nvidia-docker documentation:
+
 - https://github.com/NVIDIA/nvidia-docker
 - https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#docker
 
 ## Configuration/Installation
+
 1. Install [Docker Desktop](https://www.docker.com/products/docker-desktop).
 1. Start the Docker Daemon by searching for the program and starting it. It is useful to have docker always startup on login.
-1. Run ```docker pull s7117/ubuntu_env_cuda```
-1. Run ```docker run --gpus all --name <container_name> --hostname <hostname> -ti s7117/ubuntu_env_cuda```
+1. Run `docker pull s7117/ubuntu_env_cuda`
+1. Run `docker run --gpus all --name <container_name> --hostname <hostname> -ti s7117/ubuntu_env_cuda`
 
 ## Enter the shell via `docker exec` or `docker start`
+
 - `docker start -ai <container_name>`
 - `docker exec -u user -w /home/user -ti  <container_name> /bin/zsh`
 - `docker exec <container_name> <executable>`
 
-Optionally, you can add the ```--restart unless-stopped``` to the ```docker run``` command to restart the docker container on docker startup.
+Optionally, you can add the `--restart unless-stopped` to the `docker run` command to restart the docker container on docker startup.
