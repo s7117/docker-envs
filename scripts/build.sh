a097ec686c76b275be6722129f4a167cd3be03f3
Peyton Chandarana <peytonchandarana@gmail.com>
2025-03-07 23:53:04 -0500

    Removed -f flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,13 +6,13 @@ docker buildx create --name ubuntu_envs --use
 echo "### LOG --> Created build environment..."
 
 # Base
-docker buildx build -f ./base/20.04/Dockerfile --no-cache \
+docker buildx build ./base/20.04/Dockerfile --no-cache \
                         --platform linux/amd64,linux/arm64 \
                         --tag s7117/ubuntu-cuda:20.04
-docker buildx build -f ./base/22.04/Dockerfile --no-cache \
+docker buildx build ./base/22.04/Dockerfile --no-cache \
                         --platform linux/amd64,linux/arm64 \
                         --tag s7117/ubuntu-cuda:22.04
-docker buildx build -f ./base/latest/Dockerfile --no-cache \
+docker buildx build ./base/latest/Dockerfile --no-cache \
                         --platform linux/amd64,linux/arm64 \
                         --tag s7117/ubuntu-cuda:latest
 echo "### LOG --> Base images built..."
@@ -22,7 +22,7 @@ echo "### LOG --> Pushing images..."
 # docker push s7117/ubuntu-base:latest
 
 # Miniforge3
-docker buildx build -f ./conda/Dockerfile --no-cache \
+docker buildx build ./conda/Dockerfile --no-cache \
                         --platform linux/amd64,linux/arm64 \
                         --tag s7117/ubuntu-conda:latest
 echo "### LOG --> Conda images built..."
@@ -30,7 +30,7 @@ echo "### LOG --> Pushing images..."
 # docker push s7117/ubuntu-conda:latest
 
 # Coral TPU
-docker buildx build -f ./coraltpu/Dockerfile --no-cache \
+docker buildx build ./coraltpu/Dockerfile --no-cache \
                         --platform linux/amd64,linux/arm64 \
                         --tag s7117/ubuntu-coraltpu:latest
 echo "### LOG --> Coral TPU images built..."
@@ -39,16 +39,16 @@ echo "### LOG --> Pushing images..."
 
 # CUDA
 echo "### LOG --> BUILDING CUDA IMAGES..."
-docker buildx build -f ./cuda/11.8/Dockerfile --no-cache \
+docker buildx build ./cuda/11.8/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda:11.8
-docker buildx build -f ./cuda/12.2/Dockerfile --no-cache \
+docker buildx build ./cuda/12.2/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda:12.2
-docker buildx build -f ./cuda/12.4/Dockerfile --no-cache \
+docker buildx build ./cuda/12.4/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda:12.4
-docker buildx build -f ./cuda/12.6/Dockerfile --no-cache \
+docker buildx build ./cuda/12.6/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda:12.6
 echo "### LOG --> CUDA images built..."
@@ -60,16 +60,16 @@ echo "### LOG --> Pushing images..."
 
 # CUDA Conda
 echo "### LOG --> BUILDING GPU CONDA IMAGES..."
-docker buildx build -f ./cuda-conda/11.8/Dockerfile --no-cache \
+docker buildx build ./cuda-conda/11.8/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda-conda:11.8
-docker buildx build -f ./cuda-conda/12.2/Dockerfile --no-cache \
+docker buildx build ./cuda-conda/12.2/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda-conda:12.2
-docker buildx build -f ./cuda-conda/12.4/Dockerfile --no-cache \
+docker buildx build ./cuda-conda/12.4/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda-conda:12.4
-docker buildx build -f ./cuda-conda/12.6/Dockerfile --no-cache \
+docker buildx build ./cuda-conda/12.6/Dockerfile --no-cache \
                         --platform=linux/amd64 \
                         --tag s7117/ubuntu-cuda-conda:12.6
 echo "### LOG --> CUDA-CONDA images built..."
